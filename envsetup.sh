c1bf924d0dc9b53c191cee1766e3b3539cca1694
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2015-07-23 12:43:50 +0200

    Revert "build: Add chromium prebuilt support to envsetup.sh && The core Makefile"
    This reverts commit ce76c05847726150e16a8e5f25a6bd1720163890.
    
    Conflicts:
    	core/Makefile
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -655,15 +655,7 @@ function lunch()
 
     echo
 
-    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
-        chromium_prebuilt
-    else
-        # Unset flag in case user opts out later on
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
-    fi
-
     fixup_common_out_dir
-
     set_stuff_for_environment
     printconfig
 
@@ -2347,6 +2339,11 @@ function mk_timer()
     return $ret
 }
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> f10c6f0... Revert "build: Add chromium prebuilt support to envsetup.sh && The core Makefile"
 function make()
 {
     mk_timer $(get_make_command) "$@"
